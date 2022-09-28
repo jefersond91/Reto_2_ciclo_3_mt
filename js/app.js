60a422b5893f9edb718acae2415a7163d47b9452
Jeferson Mesa <jefersondmj21@gmail.com>
2022-09-27 21:48:33 -0500

    delete confirmation added

---
+++ js/app.js
@@ -0,0 +1,29 @@
+let newMachine = document.querySelector(".sectionForm_machine");
+let updateMachine = document.querySelector(".sectionDetails_machine");
+let tableMachine = document.querySelector(".table_machine");
+let deleteMachine = document.querySelector(".delete_final_machine");
+
+let newClient = document.querySelector(".sectionForm_client");
+let updateClient = document.querySelector(".sectionDetails_client");
+let tableClient = document.querySelector(".table_client");
+let deleteClient = document.querySelector(".delete_final_client");
+
+let newMessage = document.querySelector(".sectionForm__message");
+let updateMessage = document.querySelector(".sectionDetails_message");
+let tableMessage = document.querySelector(".table_message");
+let deleteMessage = document.querySelector(".delete_final_message");
+
+newMachine.style.display = "none";
+updateMachine.style.display = "none";
+tableMachine.style.display = "table";
+deleteMachine.style.display = "none";
+
+newClient.style.display = "none";
+updateClient.style.display = "none";
+tableClient.style.display = "table";
+deleteClient.style.display = "none";
+
+newMessage.style.display = "none";
+updateMessage.style.display = "none";
+tableMessage.style.display = "table";
+deleteMessage.style.display = "none";
