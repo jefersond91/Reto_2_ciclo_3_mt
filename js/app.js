074fa67c702aa47e96889c7f68ba695d24327bb2
Jeferson Mesa <jefersondmj21@gmail.com>
2022-10-01 20:25:05 -0500

    draft frontend finished

--- js/app.js
+++ js/app.js
@@ -1,29 +1,36 @@
 let newMachine = document.querySelector(".sectionForm_machine");
 let updateMachine = document.querySelector(".sectionDetails_machine");
+let datesMachine = document.querySelector(".dates_machine");
 let tableMachine = document.querySelector(".table_machine");
 let deleteMachine = document.querySelector(".delete_final_machine");
 
 let newClient = document.querySelector(".sectionForm_client");
 let updateClient = document.querySelector(".sectionDetails_client");
+let datesClient = document.querySelector(".dates_client");
 let tableClient = document.querySelector(".table_client");
 let deleteClient = document.querySelector(".delete_final_client");
 
 let newMessage = document.querySelector(".sectionForm__message");
 let updateMessage = document.querySelector(".sectionDetails_message");
+let datesMessage = document.querySelector(".dates_message");
 let tableMessage = document.querySelector(".table_message");
 let deleteMessage = document.querySelector(".delete_final_message");
 
-newMachine.style.display = "none";
-updateMachine.style.display = "none";
+// initial screen
+newMachine.style.display = "inline-flex";
+updateMachine.style.display = "inline-flex";
+datesMachine.style.display = "inline-flex";
 tableMachine.style.display = "table";
-deleteMachine.style.display = "none";
+deleteMachine.style.display = "inline-flex";
 
-newClient.style.display = "none";
-updateClient.style.display = "none";
+newClient.style.display = "inline-flex";
+updateClient.style.display = "inline-flex";
+datesClient.style.display = "inline-flex";
 tableClient.style.display = "table";
-deleteClient.style.display = "none";
+deleteClient.style.display = "inline-flex";
 
-newMessage.style.display = "none";
-updateMessage.style.display = "none";
+newMessage.style.display = "inline-flex";
+updateMessage.style.display = "inline-flex";
+datesMessage.style.display = "inline-flex";
 tableMessage.style.display = "table";
-deleteMessage.style.display = "none";
+deleteMessage.style.display = "inline-flex";
